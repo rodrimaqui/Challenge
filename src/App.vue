18940a8032597057d1d8a6cfaedf733af6655daf
rodrimaqui <rodri.maqui@hotmail.com>
2018-05-18 12:11:51 -0300

    add transitions

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,19 @@
     </header>
     <div class='row'>
       <div class="col-md-auto sideMenu">
+         <transition name="slide-fade">
         <div v-if='isMenuOpen'>
+          
           <rmSideMenu />
+          
           </div>
+          </transition>
         </div>  
         
         <div class='col-8'>
+          <transition name="slide-fade">
           <router-view/>
+          </transition>
         </div>
     </div>
   </div>
@@ -58,5 +64,15 @@ export default {
 </script>
 
 <style>
-
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
--- src/components/rm-allCards.vue
+++ src/components/rm-allCards.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <div class='row'>
-         <rmCard  v-for="(card,key) in getCards" :card='card' :key='card.id'/>
+            <label class="LANGUAGES-TECHNOLOGIES">LANGUAGES & TECHNOLOGIES</label> 
+        </div>
+        <div class="row">
+      
+            <rmCard  v-for="(card,key) in getCards" :card='card' :key='card.id'/>
       </div>
     </div>
 </template>
@@ -32,5 +36,19 @@
     }
 </script>
 <style>
+.LANGUAGES-TECHNOLOGIES {
+  width: 500px;
+  height: 17px;
+  font-family: Roboto;
+  font-size: 24px;
+  font-weight: 300;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1;
+  letter-spacing: 0.2px;
+  text-align: left;
+  color: #020202;
+  color: var(--black-two);
+}
 
 </style>
